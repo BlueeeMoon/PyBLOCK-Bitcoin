ffb9c39397aa861ea10cca114dff3c4e71b0d825
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2025-09-14 18:16:47 +0200

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -70,8 +70,8 @@ std::string FormatSubVersion(const std::string& name, int nClientVersion, const 
     std::string ua = strprintf("/%s:%s%s/", name, FormatVersion(nClientVersion), comments_str);
     if (!base_name_only) {
         static const auto ua_knots = []() -> std::string {
-            const auto pos{CLIENT_BUILD.find(".knots")};
-            return "Knots:" + CLIENT_BUILD.substr(pos + 6) + "/";
+            const auto pos{CLIENT_BUILD.find(".genesis")};
+            return "Genesis:" + CLIENT_BUILD.substr(pos + 6) + "/";
         }();
         ua += ua_knots;
     }
@@ -92,7 +92,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoinknots/bitcoin>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/SatoshiNakamotoBitcoin/Genesis>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR).translated + " ") + "\n" +
            "\n" +
