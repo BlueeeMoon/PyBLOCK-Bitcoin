4248c9497006e26b240fda86132a2938512f106a
BlueeeMoon <a.ireyram@hotmail.com>
2026-04-20 14:57:08 -0600

    net_processing: apply BIP110 outbound limit only to outbound full-relay peers

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3544,7 +3544,7 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
 
         pfrom.m_has_all_wanted_services = HasAllDesirableServiceFlags(nServices);
         // BIP-110: Allow up to 2 non-BIP110 outbound peers.
-        if (pfrom.ExpectServicesFromConn() && pfrom.m_conn_type != ConnectionType::MANUAL && !(nServices & NODE_REDUCED_DATA)) {
+        if (pfrom.ExpectServicesFromConn() && pfrom.m_conn_type == ConnectionType::OUTBOUND_FULL_RELAY && !(nServices & NODE_REDUCED_DATA)) {
             if (m_num_non_bip110_outbound >= 2) {
                 LogDebug(BCLog::NET, "peer lacks NODE_REDUCED_DATA and already have 2 non-BIP110 outbound peers, %s\n",
                          pfrom.DisconnectMsg(fLogIPs));
